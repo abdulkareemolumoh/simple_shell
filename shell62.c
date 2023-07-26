65180f28a8ced2aec9f21dbba169c6cc763901d1
Abdulkareem Tunde Olumoh <74350440+abdulkareemolumoh@users.noreply.github.com>
2023-07-26 23:02:36 +0100

    Update shell62.c

--- shell62.c
+++ shell62.c
@@ -1,134 +1,112 @@
 #include "string.h"
 
 /**
-  * _strnchr - get the index of the first matching character
-  * @str: string passed
-  * @chr: character passed
-  * @n: max number of characters to check
-  * Return: Index of the first occurence, or -1 chr is not found
-  */
-ssize_t _strnchr(const char *str, char chr, size_t n)
+ * strjoin - joins two strings with another string
+ * @n: address at which to store the size of the new string
+ * @sep: joining string
+ * @pre: prefix string
+ * @suf: suffix string
+ * Return: pointer to the new string
+ */
+char *strjoin(size_t *n, const char *sep, const char *pre, const char *suf)
 {
-	ssize_t index;
+	char *dest = NULL;
+	size_t sep_len = _strlen(sep ? sep : "");
+	size_t pre_len = _strlen(pre ? pre : "");
+	size_t suf_len = _strlen(suf ? suf : "");
 
-	if (!str)
-		return (-1);
-
-	for (index = 0; n && str[index]; --n, ++index)
+	dest = malloc(sizeof(char) * (pre_len + sep_len + suf_len + 1));
+	if (dest)
 	{
-		if (str[index] == chr)
-			return (index);
+		_strcpy(dest, pre ? pre : "");
+		_strcpy(dest + pre_len, sep ? sep : "");
+		_strcpy(dest + pre_len + sep_len, suf ? suf : "");
+		if (n)
+			*n = pre_len + sep_len + suf_len + 1;
 	}
-
-	return (-1);
+	return (dest);
 }
 
 
 /**
- * _strndup - duplicate the given string
- * @str: the string to duplicate
- * @n: the max number of bytes to copy
- *
- * Description: This function copies at most n bytes. If str is longer
- * than n, only n bytes are copied, and a terminating null byte is added.
- *
- * Return: If str is NULL or if memory allocation fails, return NULL.
- * Otherwise a return a pointer to the dynamically-allocated duplicate.
+ * strjoina - join strings from a NULL-terminated array
+ * @n: address at which to store the length of the new string
+ * @sep: joining string
+ * @array: array of strings
+ * Return: pointer to the new string
  */
-char *_strndup(const char *str, size_t n)
+char *strjoina(size_t *n, const char *sep, const char **array)
 {
-	char *dup;
-	size_t len = 0;
+	char *dest = NULL;
+	size_t len = 0, idx = 0, sep_len = _strlen(sep ? sep : "");
 
-	if (!str)
-		return (NULL);
+	while (array[idx])
+		len += _strlen(array[idx++]);
 
-	while (n && str[len])
-		--n, ++len;
-
-	dup = malloc(sizeof(char) * (len + 1));
-	if (!dup)
-		return (NULL);
-
-	dup[len] = '\0';
-
-	while (len--)
-		dup[len] = str[len];
-
-	return (dup);
-}
-
-
-/**
- * _strnlen - calculate the length of a string
- * @str: the string to measure
- * @n: the max number of characters to check
- * Return: the lesser of n and the length of the string
- */
-ssize_t _strnlen(const char *str, size_t n)
-{
-	const char *pos = str;
-
-	if (!str)
-		return (-1);
-
-	while (n && *pos)
-		--n, ++pos;
-
-	return (pos - str);
-}
-
-
-
-/**
- * _strncmp - compare two strings
- * @s1: a string to compare
- * @s2: the other string to compare
- * @n: the max number of bytes to compare
- * Return: 0 if s1 matches s2,
- * otherwise an integer less than 0 if s1 is less than s2,
- * otherwise an integer greater than 0 if s1 is greater than s2.
- */
-int _strncmp(const char *s1, const char *s2, size_t n)
-{
-	for (; n && *s1 && *s2; --n, ++s1, ++s2)
+	if (idx--)
 	{
-		if (*s1 != *s2)
-			return (*s1 - *s2);
+		dest = malloc(sizeof(char) * (len + idx * sep_len + 1));
+		if (dest)
+		{
+			len = 0;
+			while (*array)
+			{
+				_strcpy(dest + len, *array);
+				len += _strlen(*array);
+				if (sep_len && idx--)
+				{
+					_strcpy(dest + len, sep);
+					len += sep_len;
+				}
+			}
+			if (n)
+				*n = len + 1;
+		}
 	}
-
-	if (n)
-	{
-		if (*s1)
-			return (1);
-		if (*s2)
-			return (-1);
-	}
-
-	return (0);
+	return (dest);
 }
 
 
 /**
- * _strncpy - copy the string
- * @dest: destination
- * @src: source
- * @n: the max number of bytes to copy
- *
- * Description: This function copies at most n bytes from src to dest. A
- * null byte will NOT be written if not found in the first n bytes
- *
- * Return: a pointer to dest
+ * strjoinl - joins a NULL terminated list of strings with a character
+ * @n: address at which to store the length of the new string
+ * @sep: joining string
+ * @...: strings
+ * Return: pointer to the new string
  */
-
-char *_strncpy(char *dest, const char *src, size_t n)
+char *strjoinl(size_t *n, const char *sep, ...)
 {
-	char *pos = dest;
+	char *dest = NULL;
+	const char *temp = NULL;
+	size_t idx = 0, len = 0, sep_len = _strlen(sep ? sep : "");
+	va_list ap;
 
-	for ( ; n && *src; --n)
-		*pos++ = *src++;
-	if (n)
-		*pos = '\0';
+	for (va_start(ap, sep); (temp = va_arg(ap, char *)); idx += 1)
+		len += _strlen(temp);
+	va_end(ap);
 
+	if (idx--)
+	{
+		dest = malloc(sizeof(char) * (len + idx * sep_len + 1));
+		if (dest)
+		{
+			len = 0;
+			va_start(ap, sep);
+			while ((temp = va_arg(ap, char *)))
+			{
+				_strcpy(dest + len, temp);
+				len += _strlen(temp);
+				if (sep_len && idx--)
+				{
+					_strcpy(dest + len, sep);
+					len += sep_len;
+				}
+			}
+			va_end(ap);
+			if (n)
+				*n = len + 1;
+		}
+
+	}
 	return (dest);
 }
